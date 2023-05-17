54383da92c70d35e11b6981b0ebc63e8e8e9f8a9
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2023-05-17 16:56:18 +0200

    HW 6 done

--- models/users.js
+++ models/users.js
@@ -1,6 +1,8 @@
 import User from "./userModel.js";
 import bcrypt from "bcrypt";
 import gravatar from "gravatar";
+import { nanoid } from "nanoid";
+import { sendVerificationEmail } from "../middlewares/sendgrid.js";
 
 const hashPassword = async (pwd) => {
   const salt = await bcrypt.genSaltSync(10);
@@ -13,13 +15,20 @@ const validatePassword = (pwd, hash) => bcrypt.compare(pwd, hash);
 export const createUser = async (email, password) => {
   try {
     const avatarURL = gravatar.url(email, {
-      s: 250,
-      r: pg,
-      d: retro,
+      s: "250",
+      r: "pg",
+      d: "retro",
     });
     const hashedPassword = await hashPassword(password);
-    const newUser = new User({ email, password: hashedPassword, avatarURL });
+    const verificationToken = nanoid();
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      avatarURL,
+      verificationToken,
+    });
     const user = await newUser.save();
+    await sendVerificationEmail(email, verificationToken);
     return user;
   } catch (err) {
     console.log(err);
@@ -52,3 +61,9 @@ export const passwordValidator = async (password, userPassword) => {
 export const updateAvatar = async (email, avatarURL) => {
   User.findOneAndUpdate({ email }, { avatarURL });
 };
+
+export const verifyUser = (verificationToken) =>
+  User.findOneAndUpdate(
+    { verificationToken },
+    { verify: true, verificationToken: null }
+  );
